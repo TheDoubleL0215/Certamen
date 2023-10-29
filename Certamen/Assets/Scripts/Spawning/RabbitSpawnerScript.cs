f9af2419c8161370d7cddf46e42288d74579ff6f
Gyulaa <ii.miklosgyula@gmail.com>
2023-10-29 07:43:51 +0100

    Nyulak spawnolása, Földsík, Mappaszerkezet

---
+++ Certamen/Assets/Scripts/Spawning/RabbitSpawnerScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RabbitSpawnerScript : MonoBehaviour
+{
+    // Kiválasztjuk melyik karaktert spawnolja
+    public GameObject Rabbit;
+    // Ettõl függ, hány nyul spawnol a legelején.
+    public int startAmount;
+    // Megadjuk a kordinátákat, amin belül spawnolnak a nyulak
+    public float lowestX;
+    public float highestX;
+    public float lowestZ;
+    public float highestZ;
+    // Megadott fokokon belül véletlen irány a kezdésnél
+    private float minRotation = 0f;
+    private float maxRotation = 90f;
+
+    void Start()
+    {
+        // 'startAmount'-szor spawnol egy nyulat a 'spawnRabbit()' függvényt meghívva
+        for (int i = 0; i < startAmount; i++)
+        {
+            spawnRabbit();
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    // A bekért paraméterek szerint spawnol egy fûcsomót
+    void spawnRabbit()
+    {
+        Instantiate(Rabbit, new Vector3(Random.Range(lowestX, highestX), 0.19f, Random.Range(lowestZ, highestZ)), transform.rotation);
+        float randomRotation = Random.Range(minRotation, maxRotation);
+        transform.Rotate(Vector3.up, randomRotation);
+    }
+}
